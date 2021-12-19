1181b3d6d370e0089f360972e750872b0dd2c793
PetyaWebDesign <petyawebdesign@gmail.com>
2021-12-19 10:20:08 +0200

    added the first content block and implemented scrollTrigger to it

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,10 @@
-const mountainRange = document.querySelector('.moon img');
-const title = document.querySelector('h1')
+const moonContent = document.querySelector('.moon img');
+const title = document.querySelector('.moon-title');
+const blockTitleOne = document.querySelector('h2');
 
-gsap.to(mountainRange, {
+
+// entry section animation
+gsap.to(moonContent, {
     y: 100,
     scrollTrigger: {
         trigger: 'moon',
@@ -12,7 +15,20 @@ gsap.to(mountainRange, {
 gsap.to(title, {
     y: 500,
     scrollTrigger: {
-        trigger: "moon",
+        trigger: 'moon',
         scrub: true
+    }
+})
+
+// first content block
+gsap.to(blockTitleOne, {
+    duration: 1,
+    scale: 0.5,
+    y: 200,
+    autoAlpha: 0,
+    scrollTrigger: {
+        trigger: '.content-block-1 h2',
+        scrub: true,
+        start: 'top 50%'
     }
 })
